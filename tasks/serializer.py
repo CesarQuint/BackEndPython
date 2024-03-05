16abcc3557abe13b92cd1c95547f388877992b8e
CesarQuint <pedrocesarquintero001@gmail.com>
2024-03-05 12:25:54 -0600

    Creation of a simple CRUD

--- tasks/serializer.py
+++ tasks/serializer.py
@@ -3,6 +3,6 @@ from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model:Task
+        model=Task
         # fields = ('id','title','description','done')
         fields = ('__all__')
