9b47bda43e9b18882e9af2665a7e09b9adcab276
CesarQuint <pedrocesarquintero001@gmail.com>
2024-03-09 14:43:53 -0600

    update manager

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -131,6 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = []
 
 REST_FRAMEWORK = {
-    ...: ...,
+   
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
